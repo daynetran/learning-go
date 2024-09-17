289bf38918325d5ddbb012cc9405772c57d71149
Dayne Tran <daynetran256@gmail.com>
2024-09-17 15:57:05 -0700

    basics 03-operators

--- practice/basics/03-Operators/main.go
+++ practice/basics/03-Operators/main.go
@@ -1,1 +1,54 @@
 package main
+
+import "fmt"
+
+func main() {
+	i := 10
+	k := 20
+	z := 30
+
+	// Arithmetic Operators
+	fmt.Printf("i + k = %d\n", i+k)
+	fmt.Printf("i - k = %d\n", i-k)
+	fmt.Printf("i * k = %d\n", i*k)
+	fmt.Printf("i / k = %d\n", i/k)
+	fmt.Printf("i mod k = %d\n", i%k)
+
+	// Comparison Operators
+	fmt.Println(i == k)
+	fmt.Println(i != k)
+	fmt.Println(i < k)
+	fmt.Println(i <= k)
+	fmt.Println(i > k)
+	fmt.Println(i >= k)
+
+	// Logical Operators
+	fmt.Println(i < z && i > k)
+	fmt.Println(i < z || i > k)
+	fmt.Println(!(i == z && i > k))
+
+	// Assigment Operators
+	var x, y = 15, 25
+	x = y
+	fmt.Println("= ", x)
+
+	x = 15
+	x += y
+	fmt.Println("+=", x)
+
+	x = 50
+	x -= y
+	fmt.Println("-=", x)
+
+	x = 2
+	x *= y
+	fmt.Println("*=", x)
+
+	x = 100
+	x /= y
+	fmt.Println("/=", x)
+
+	x = 40
+	x %= y
+	fmt.Println("%=", x)
+}
